4cbb15ea7390e2f28787a9dfe983f1e264acf23c
GabrielKupatadze <kupatagabro@gmail.com>
2024-05-25 14:49:01 +0400

    first commit

---
+++ Level 3/Homework/level3hw.py
@@ -0,0 +1,18 @@
+first_name = "Gabriel" # A Variable where it shows my first name.
+last_name = "Kupatadze" # A Variable where it shows my last name.
+age = 13 # A Variable where it shows my age.
+hobby = "Computer Science/Driving" # A Variable where it shows what hobbys i am passionate about.
+country = "Georgia" # A Variable that shows where i am from.
+
+print(first_name, last_name, age, hobby, country)
+
+print(10 + 10)
+print(110 - 10)
+print(10 * 10)
+print(10 ** 10)
+print(100 / 10)
+print(100 // 10)
+print(2100 // 5)
+print(120 + 80)
+print(8 ** 11)
+print(1000 // 10)
